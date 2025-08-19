630b45a0a5634efb8ff202ced13108c7aee6f93d
Heriberto Sanchez <hersac1@gmail.com>
2025-08-19 02:03:03 -0500

    Crea proyecto y su estructura incial

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const title = document.querySelector('#title');
+const app = document.querySelector('#app');
+const header = document.querySelector('header');
+const footer = document.querySelector('#footer');
+
+title.textContent = 'Heriberto | Desarrollador Web';
